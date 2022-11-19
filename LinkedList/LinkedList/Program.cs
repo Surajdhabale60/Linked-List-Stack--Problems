844d34dd32888f024d3815011fac631d8ddabf81
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-19 20:51:30 +0530

    [Suraj]Add:Creating LinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace  LinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
