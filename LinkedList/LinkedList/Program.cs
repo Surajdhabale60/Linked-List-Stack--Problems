f483d63cd6b6639bbf9061689a1ce63f21194412
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-23 16:15:43 +0530

    [Suraj]Add:Removing Last Element

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -13,11 +13,13 @@
             //linkedList.AddInReverseOrder(30);
             //linkedList.AddInReverseOrder(56);
 
-            //linkedList.Add(56);
-            //linkedList.Add(70);
+            linkedList.Add(56);
+            linkedList.Add(70);
+            linkedList.Add(30);
             //linkedList.InsertAtParticularPosition(1,30);
 
             linkedList.RemoveFirstNode();
+            linkedList.RemoveLastNode();
 
             linkedList.Display();
         }
